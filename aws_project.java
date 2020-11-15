2176a57b5ab3f8b3900154e34cd122ee43caebe0
mkjang0905@naver.com <mkjang0905@naver.com>
2020-11-15 18:31:34 +0900

    8 list complete

--- aws_project.java
+++ aws_project.java
@@ -8,13 +8,36 @@ import com.amazonaws.services.ec2.model.RunInstancesResult;
 import com.amazonaws.services.ec2.model.Tag;
 
 //list Instances
-import com.amazonaws.services.ec2.AmazonEC2;
-import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
 import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
 import com.amazonaws.services.ec2.model.DescribeInstancesResult;
 import com.amazonaws.services.ec2.model.Instance;
 import com.amazonaws.services.ec2.model.Reservation;
 
+//available zones
+import com.amazonaws.services.ec2.model.DescribeRegionsResult;
+import com.amazonaws.services.ec2.model.Region;
+import com.amazonaws.services.ec2.model.AvailabilityZone;
+import com.amazonaws.services.ec2.model.DescribeAvailabilityZonesResult;
+
+//start instances
+import com.amazonaws.services.ec2.model.StartInstancesRequest;
+
+//available regions
+import com.amazonaws.services.ec2.model.DescribeRegionsResult;
+import com.amazonaws.services.ec2.model.Region;
+import com.amazonaws.services.ec2.model.AvailabilityZone;
+import com.amazonaws.services.ec2.model.DescribeAvailabilityZonesResult;
+
+//stop instances
+import com.amazonaws.services.ec2.model.StopInstancesRequest;
+
+//reboot instances
+import com.amazonaws.services.ec2.model.RebootInstancesRequest;
+import com.amazonaws.services.ec2.model.RebootInstancesResult;
+
+//list images
+import com.amazonaws.services.ec2.model.Image;
+import com.amazonaws.services.ec2.model.DescribeImagesRequest;
 
 public class aws_project{
 
@@ -75,7 +98,7 @@ public class aws_project{
 		        System.out.print("Enter an integer: ");
 
 			num = menu.nextInt();
-			menu.netLine();
+			menu.nextLine();
 
 			switch(num){
 				case 1:
@@ -113,7 +136,7 @@ public class aws_project{
 	private static void listInstances(){
 	
 		System.out.println("Listing instances....");
-		booleandone = false;
+		boolean done = false;
 		DescribeInstancesRequestrequest = new DescribeInstancesRequest();
 		while(!done) {
 			DescribeInstancesResultresponse = ec2.describeInstances(request);
@@ -133,37 +156,104 @@ public class aws_project{
 	//2. available zones
 	private static void availableZones(){
 	
-	
+		DescribeAvailabilityZonesResult zones_response = ec2.describeAvailabilityZones();
+      		for (AvailabilityZone zone : zones_response.getAvailabilityZones()) {
+         		System.out.printf("[id] %s, " + "[region] %s, " + "[zone] %s, ", zone.getZoneId(), zone.getRegionName(),zone.getZoneName());
+         		System.out.println();
+		}
 	}
 
 	//3. start instances
 	private static void startInstances(){
+		Scanner scanner = new Scanner(System.in);
+		System.out.printf("Enter instance id :");
+		String instance_id = scanner.nextLine();
 
+		System.out.printf("Starting instances.... %s", instance_id);
+
+		try{
+			StartInstancesRequest request = new StartInstancesRequest().withInstanceIds(instance_id);
+		        ec2.startInstances(request);
+		        System.out.println();
+		        System.out.printf("Successfully started instance %s", instance_id);
+          		System.out.println();
+		}catch(Exception e){
+			throw new AmazonClientException("You cannot create an instance. Check the value you entered", e);
+		}
 	}
 
 	//4. available regions
 	private static void availableRegions(){
-	
+		DescribeRegionsResult regions_response = ec2.describeRegions();
+
+      		for (Region region : regions_response.getRegions()) {
+         		System.out.printf("[region] %s, " + "[endpoint] %s, ", region.getRegionName(), region.getEndpoint());
+         		System.out.println();
+		}
 	}
 
 	//5. stop instances
 	private static void stopInstances(){
-	
+		Scanner scanner = new Scanner(System.in);
+                System.out.printf("Enter instance id :");
+                String instance_id = scanner.nextLine();
+
+		try{
+			StopInstancesRequest request = new StopInstancesRequest().withInstanceIds(instance_id);
+		        ec2.stopInstances(request);
+		        System.out.printf("Successfully stoped instance %s", instance_id);
+		}catch(Exception e){
+			throw new AmazonClientException("You cannot create an instance. Check the value you entered", e);
+		}
 	}
 
 	//6. create instances
 	private static void createInstances(){
-	
+		Scanner scanner = new Scanner(System.in);
+		System.out.printf("Enter ami id :");
+		String ami_id = scanner.nextLine();
+
+		try{
+			RunInstancesRequest run_request = new RunInstancesRequest().withImageId(ami_id).withInstanceType(InstanceType.T2Micro).withMaxCount(1).withMinCount(1).withKeyName(key_id);
+
+			RunInstancesResult run_response = ec2.runInstances(run_request);
+			String reservation_id = run_response.getReservation().getInstances().get(0).getInstanceID();
+			System.out.printf("Successfully started EC2 instance %s based on AMI %s", reservation_id, ami_id);
+		}catch(Exception e){
+			throw new AmazonClientException("You cannot create an instance. Check the value you entered", e);
+		}
 	}
 
 	//7. reboot instances
 	private static void rebootInstances(){
-	
+		Scanner scanner = new Scanner(System.in);
+                System.out.printf("Enter ami id :");
+                String ami_id = scanner.nextLine();	
+		System.out.printf("Rebooting .... %s", instance_id);
+
+		try{
+			RebootInstancesRequest request = new RebootInstancesRequest().withInstanceIds(instance_id);
+			RebootInstancesResult response = ec2.rebootInstances(request);
+			System.out.println();
+		        System.out.printf("Successfully rebooted instance %s", instance_id);	
+		}catch(Exception e){
+			throw new AmazonClientException("You cannot create an instance. Check the value you entered", e);
+		}
 	}
 
 	//8. list images
 	private static void listImages(){
-	
+		DescribeImagesRequest request = new DescribeImagesRequest().withOwners("self");
+      		Collection<Image> images = ec2.describeImages(request).getImages();
+
+      		int flag_ami = 1;
+
+      		for (Image Im : images) {
+         		System.out.println(flag_ami + ")");
+         		System.out.printf("[Image ID] %s, " + "Owner ID] %s, "+ "[AMI Status] %s, ",Im.getImageId(), Im.getOwnerId(),Im.getState());
+         		flag_ami++;
+         		System.out.println();
+		}		
 	}
 
 }
